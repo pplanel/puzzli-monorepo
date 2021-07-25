cca00941bee805e97f5feae6691e8e48be678dc8
Pedro Gabriel Planel <pplanel@users.noreply.github.com>
2021-07-25 05:28:27 +0000

    Limpeza do projeto (#1)
    Estruturacão de Monorepo  V0.0.1 - Inicio Puzzli Estruturado.

--- apps/frontend/react-front/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const { override } = require("customize-cra");
-const { aliasDangerous } = require("react-app-rewire-alias/lib/aliasDangerous");
-
-const aliasMap = {
-  "web-components-react": "../web-components-react",
-};
-
-module.exports = override(aliasDangerous(aliasMap));
