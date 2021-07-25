5ed3a5a7b30b303404083f3c238e75cdc2606af9
Pedro Gabriel Planel <pedro.planel@puzzl.com.br>
2021-07-25 02:29:04 +0000

    TASK(pplanel): created nestjs backend

--- apps/backend/nestjs/src/main.ts
+++ apps/backend/nestjs/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ArgumentOutOfRangeError } from 'rxjs';
-import { Application } from './Application';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(Application);
-  app.enableCors()
+  const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
 bootstrap();
